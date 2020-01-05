b03ba617a14a388a9b6fbefeb4272ad52a00cfd2
amaraphon <57031249+amaraphon@users.noreply.github.com>
2020-01-05 14:03:10 +0700

    Palette na

---
+++ Palette/app/src/main/java/th/ac/su/ict/amaraphon/palette/MainActivity.kt
@@ -0,0 +1,27 @@
+package th.ac.su.ict.amaraphon.palette
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val baseBt      = findViewById<Button>(R.id.baseBt)
+        val pinkBt      = findViewById<Button>(R.id.pinkBt)
+        val dIndigoBt   = findViewById<Button>(R.id.dIndigoBt)
+        val mdIndigoBt  = findViewById<Button>(R.id.mdIndigoBt)
+        val lowIndigoBt = findViewById<Button>(R.id.lowIndigoBt)
+        val blueBt      = findViewById<Button>(R.id.blueBt)
+
+
+
+
+
+
+
+    }
+}
